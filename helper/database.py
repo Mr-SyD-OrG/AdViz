8dac6043914a6c4e3a3c3f48a00d02e154a1e25b
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-16 17:43:17 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -16,14 +16,7 @@ class Database:
         self.batches = self.db.batches
         self.active_batches = self.db.active_batches
 
-    async def find_join_req(self, id):
-        return bool(await self.req.find_one({'id': id}))
-        
-    async def add_join_req(self, id):
-        await self.req.insert_one({'id': id})
-        
-    async def del_join_req(self):
-        await self.req.drop()
+
         
     def new_user(self, id):
         return dict(
