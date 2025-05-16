e41208269e7b8dba9b64ed8f93ed61d1fb63baa9
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-16 06:48:02 +0530

    Update features.py

--- plugins/features.py
+++ plugins/features.py
@@ -3,6 +3,19 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, 
 from helper.database import db
 import re
 
+# Pattern 1: S01E02 or S01EP02
+pattern1 = re.compile(r'S(\d+)(?:E|EP)(\d+)')
+# Pattern 2: S01 E02 or S01 EP02 or S01 - E01 or S01 - EP02
+pattern2 = re.compile(r'S(\d+)\s*(?:E|EP|-\s*EP)(\d+)')
+# Pattern 3: Episode Number After "E" or "EP"
+pattern3 = re.compile(r'(?:[([<{]?\s*(?:E|EP)\s*(\d+)\s*[)\]>}]?)')
+# Pattern 3_2: episode number after - [hyphen]
+pattern3_2 = re.compile(r'(?:\s*-\s*(\d+)(?!p)\s*)')
+# Pattern 4: S2 09 ex.
+pattern4 = re.compile(r'S(\d+)\s*[-E ]\s*(\d+)', re.IGNORECASE)
+# Pattern X: Standalone Episode Number
+patternX = re.compile(r'\b(?!\d{3,4}p\b)\d{3,4}\b', re.IGNORECASE)
+
 def extract_episode_number(filename):    
     match = re.search(pattern1, filename)
     if match:
