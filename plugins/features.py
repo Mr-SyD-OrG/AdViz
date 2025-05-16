3fac5e4fcbee467f738b4a055f49dfe290283ba2
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-16 06:47:04 +0530

    Update features.py

--- plugins/features.py
+++ plugins/features.py
@@ -1,8 +1,45 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 from helper.database import db
+import re
 
+def extract_episode_number(filename):    
+    match = re.search(pattern1, filename)
+    if match:
+        print("Matched Pattern 1")
+        return match.group(2)  # Extracted episode number
+    
+    # Try Pattern 2
+    match = re.search(pattern2, filename)
+    if match:
+        print("Matched Pattern 2")
+        return match.group(2)  # Extracted episode number
 
+    # Try Pattern 3
+    match = re.search(pattern3, filename)
+    if match:
+        print("Matched Pattern 3")
+        return match.group(1)  # Extracted episode number
+
+    # Try Pattern 3_2
+    match = re.search(pattern3_2, filename)
+    if match:
+        print("Matched Pattern 3_2")
+        return match.group(1)  # Extracted episode number
+        
+    # Try Pattern 4
+    match = re.search(pattern4, filename)
+    if match:
+        print("Matched Pattern 4")
+        return match.group(2)  # Extracted episode number
+
+    # Try Pattern X
+    match = re.search(patternX, filename)
+    if match:
+        print("Matched Pattern X")
+        return match.group(0)  # Extracted episode number
+        
+    return "None"
 async def features_button(user_id):
     metadata = await db.get_metadata(user_id)
 
